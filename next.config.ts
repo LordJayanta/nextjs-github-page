7fabf0e7e55e84f6e04160d93da6ec95a1216689
LordJayanta <techie.jayanta@gmail.com>
2026-07-06 20:32:38 +0530

    fix basepath

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  basePath: '/nextjs-github-page',
+  basePath: isProd ? '/nextjs-github-page' : '',
+  assetPrefix: isProd ? '/nextjs-github-page/' : '',
   images: {
     unoptimized: true,
   }
