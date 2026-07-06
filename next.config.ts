e5e4fdaadcc1c3bde0e239c52f0eb414cd91da56
LordJayanta <techie.jayanta@gmail.com>
2026-07-06 19:43:15 +0530

    fix image 404 error

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  basePath: '/nextjs-github-page',
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
